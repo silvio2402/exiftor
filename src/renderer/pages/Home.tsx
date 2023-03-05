275bf26a4c4e09193d43f0952a3da5e0e85981ce
Silvio Brändle <silvio@silviobraendle.ch>
2023-03-05 21:30:51 +0100

    Use wrapper div for home page

--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -1,14 +1,15 @@
 import { Typography } from 'antd';
+import indexStyles from './index.module.scss';
 // import Image from '../components/Image';
 
 const Home = () => {
   return (
-    <>
+    <div className={indexStyles.wrapper}>
       <Typography.Title>Home</Typography.Title>
       <Typography.Paragraph>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
       </Typography.Paragraph>
-    </>
+    </div>
   );
 };
 
