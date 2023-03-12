007963eab44743398584d1dce7d0a41d6e4d084b
Silvio Brändle <silvio@silviobraendle.ch>
2023-03-12 19:07:39 +0100

    Fix test "not wrapped in act" error

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,13 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import App from '../renderer/App';
 
 describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+  it('should render', async () => {
+    let component;
+    await act(async () => {
+      component = render(<App />);
+    });
+    expect(component).toBeTruthy();
   });
 });
