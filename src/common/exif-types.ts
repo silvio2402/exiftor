19e852fdb039d49912719236f651d2fa9aa4ec83
Silvio Brändle <silvio@silviobraendle.ch>
2023-06-11 22:22:35 +0200

    Fix actions build

--- src/common/exif-types.ts
+++ src/common/exif-types.ts
@@ -2,9 +2,8 @@ import { z } from 'zod';
 import { RawTags } from 'exiftool-vendored';
 import { jsonSchemaNoNull } from './json-types';
 
-const rawTagsSchema: z.ZodType<RawTags> = z.record(
+// eslint-disable-next-line import/prefer-default-export
+export const rawTagsSchema: z.ZodType<RawTags> = z.record(
   z.string(),
   jsonSchemaNoNull
 );
-
-export default { rawTagsSchema, jsonSchemaNoNull };
